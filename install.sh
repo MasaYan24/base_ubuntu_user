d32bbf72e34e0ccf203f55c1aafe6e618123ec84
Masahiro Takahashi <masahiro.takahashi.j7x@jp.denso.com>
2023-02-21 01:55:15 +0900

    update: install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,37 @@
 #!/bin/sh
 
-# install shell and basic utilities
+## install shell and basic utilities
 sudo apt install -y zsh unzip ruby openssh-server curl cifs-utils
 sudo apt install -y gcc make pkg-config libglvnd-dev  # for NVIDIA driver
 sudo apt install -y tmux
 sudo chsh -s /bin/zsh $(whoami)
 
-# install homebrew
+## install homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Neovim install and setting
-sudo apt install -y neovim
-git clone https://github.com/MasaYan24/.vim.git $HOME/
-sh $HOME/.vim/install.sh
+## Neovim install and setting
+# sudo apt install -y neovim
+# git clone https://github.com/MasaYan24/.vim.git $HOME/
+# sh $HOME/.vim/install.sh
 
-# Prompto setting
-# sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+## Prompto setting
 brew install starship
 mkdir -p $HOME/.config && echo "command_timeout = 2000" > $HOME/.config/starship.toml
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 wget https://raw.githubusercontent.com/MasaYan24/zshrc/main/.zshrc -P $HOME/
 
-# Developing tool
+## Developing tool
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
   -O /tmp/miniconda.sh \
   && sh /tmp/miniconda.sh -b -p $HOME/miniconda
 
-# git setting
-git config --global core.editor vi
-git config --global merge.tool vimdiff
-git config --global merge.conflictstyle diff3
+## git setting
+git config --global core.editor nano
 git config --global mergetool.prompt false
 git config --global credential.helper "cache --timeout 604800"
 git config --global core.pager "LESSCHARSET=utf-8 less"
 
-# Python formatter
+## Python formatter
 conda activate base
 pip install black isort flake8 autopep8
 

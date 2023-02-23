6c24b615e7390e56442832ed6af5b7beffeaba18
Masahiro Takahashi <m.takahashi.public+github@gmail.com>
2023-02-23 11:04:38 +0900

    update: for local user

--- install.sh
+++ install.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
-
-## install shell and basic utilities
-sudo apt install -y zsh unzip ruby openssh-server curl cifs-utils
-sudo apt install -y gcc make pkg-config libglvnd-dev  # for NVIDIA driver
-sudo apt install -y tmux
-sudo chsh -s /bin/zsh $(whoami)
+set -e
 
 ## install homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## Neovim install and setting
-# sudo apt install -y neovim
 # git clone https://github.com/MasaYan24/.vim.git $HOME/
 # sh $HOME/.vim/install.sh
 
 ## Prompto setting
-brew install starship
+curl -sS https://starhip.rs/install.sh | sh
 mkdir -p $HOME/.config && echo "command_timeout = 2000" > $HOME/.config/starship.toml
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 wget https://raw.githubusercontent.com/MasaYan24/zshrc/main/.zshrc -P $HOME/
@@ -23,17 +17,11 @@ wget https://raw.githubusercontent.com/MasaYan24/zshrc/main/.zshrc -P $HOME/
 ## Developing tool
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
   -O /tmp/miniconda.sh \
-  && sh /tmp/miniconda.sh -b -p $HOME/miniconda
+  && sh /tmp/miniconda.sh -b -p $HOME/.miniconda
+echo 'export PATH=$HOME/.miniconda/bin:$PATH' >> $HOME/.zshrc
 
 ## git setting
 git config --global core.editor nano
 git config --global mergetool.prompt false
 git config --global credential.helper "cache --timeout 604800"
 git config --global core.pager "LESSCHARSET=utf-8 less"
-
-## Python formatter
-conda activate base
-pip install black isort flake8 autopep8
-
-# Update all
-sudo apt upgrade -y && echo "system restarting!" && sudo reboot
